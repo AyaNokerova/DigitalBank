f3248e5f5ae8508d3722091c2667f31049d6aaa9
AyaNokerova <nokerova.a@gmail.com>
2022-08-15 11:41:38 -0400

    added New Checking Account feature, changed open.website() method and put it inside the Driver class, since it was reperating in all features

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -28,17 +29,49 @@ public class Login extends BasePage {
     @FindBy(xpath = "//a[contains(text(),' Sign Up Here')]")
     WebElement signUpHereLink;
 
+    @FindBy(xpath ="//div[@class='sufee-alert alert with-close alert-danger alert-dismissible fade show']")
+    WebElement loginErrorMessage;
 
-    public void openWebsite() {
-        driver.get(ConfigReader.getProperty("app.baseurl"));
-    }
 
     public void goToSignUpPage(){
 
         signUpHereLink.click();
 
     }
+    public void verifyPageTitle() {
+        Assert.assertEquals("Title is not the same", "Digital Bank", Driver.getDriver().getTitle());
+    }
 
+    public void verifySignInButtonIsDisplayed() {
+        Assert.assertTrue(signInButton.isDisplayed());
+    }
+
+
+    public void doLogin() {
+
+        username.sendKeys(ConfigReader.getProperty("valid.username") );
+        password.sendKeys(ConfigReader.getProperty("valid.password"));
+
+    }
+
+    public void doLogin(String invalidUsername, String invalidPassword) {
+
+        username.sendKeys(invalidUsername);
+        password.sendKeys(invalidPassword + Keys.ENTER);
+    }
+
+    public void clickOnSignInButton() {
+        signInButton.click();
+    }
+    public void verifyUserIsOnLoginPage() {
+
+        Assert.assertTrue("User is not on the Home Page", signUpHereLink.isDisplayed());
+    }
+    public void verifyLoginErrorMessageIsDisplayed(){
+
+        Assert.assertTrue( loginErrorMessage.isDisplayed());
+        System.out.println(loginErrorMessage.getText());
+    }
 }
 
 
--- src/test/java/stepDefs/LoginSteps.java
+++ src/test/java/stepDefs/LoginSteps.java
@@ -5,53 +5,61 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
+import pages.HomePage;
 import pages.Login;
 
 public class LoginSteps extends Login{
 
     Login login = new Login();
+    HomePage homePage = new HomePage();
 
     @Given("user entered correct URL")
     public void user_entered_correct_url() {
-        login.openWebsite();
+
     }
 
     @Then("verify the title of the webpage is Digital Bank")
     public void verify_the_title_of_the_webpage_is_digital_bank() {
+        login.verifyPageTitle();
     }
 
     @Then("verify all sign-in text boxes and buttons are present")
     public void verify_all_sign_in_text_boxes_and_buttons_are_present() {
+        login.verifySignInButtonIsDisplayed();
     }
 
     @Given("user enters valid username {string} and password {string}")
     public void user_enters_valid_username_and_password(String string, String string2) {
+
+            login.doLogin();
     }
 
     @Then("verify user is successfully logged in to the account")
-    public void verify_user_is_successfully_logged_in_to_the_account() {
+    public void verify_user_is_successfully_logged_in_to_the_account()
+  {
+        homePage.verifyUserIsSignedInSuccessfully();
     }
+
 
     @Given("user enters invalid username {string} and password {string}")
     public void user_enters_invalid_username_and_password(String string, String string2) {
-        login.openWebsite();
+
         username.sendKeys(string + Keys.ENTER);
         password.sendKeys(string2 + Keys.ENTER);
     }
 
     @When("user clicks on Sign In button")
     public void user_clicks_on_sign_in_button() {
-        signInButton.click();
+      login.clickOnSignInButton();
     }
 
     @Then("verify user login failed with an error message")
     public void verify_user_login_failed_with_an_error_message() {
-        Assert.assertTrue("User haven't received error message", errorMessage.isDisplayed());
+        login.verifyLoginErrorMessageIsDisplayed();
     }
 
     @Given("user enters valid username {string} and invalid password {string}")
     public void user_enters_valid_username_and_invalid_password(String string, String string2) {
-        login.openWebsite();
         username.sendKeys(string + Keys.ENTER);
         password.sendKeys(string2 + Keys.ENTER);
     }
