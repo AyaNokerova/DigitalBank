1e425a177d92629995b41fc308ac8a03fb82dede
M1keFreeman <bayacondios1@gmail.com>
2022-08-25 21:17:13 -0500

    log4j

--- src/test/java/pages/JDBCTest.java
+++ src/test/java/pages/JDBCTest.java
@@ -1,5 +1,6 @@
 package pages;
 
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,27 +8,32 @@ import org.junit.Test;
 import utilities.DataBaseUtils;
 
 import java.sql.*;
+import java.util.logging.Logger;
 
 public class JDBCTest {
     private static Connection connection;
     private static Statement statement;
     private static final String JDBC_URL = "jdbc:mysql://3.129.60.236:3306/digitalbank?user=digitaluser&password=Demo123!";
+   static Logger log = Logger.getLogger(JDBCTest.class.getName());
+     // static Logger log = Logger.getLogger("JDBCTest");
 
-
-//    public void initDatabase() throws Exception {
-//        // 1. Connect to the Database
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        con = DriverManager.getConnection("3.129.60.236:3306", "digitaluser", "Demo123!");
+    //    public void initDatabase() throws Exception {
+    //        // 1. Connect to the Database
+    //        Class.forName("com.mysql.cj.jdbc.Driver");
+    //        con = DriverManager.getConnection("3.129.60.236:3306", "digitaluser", "Demo123!");
         @Before
         public void open() {
+            log.info("Connection is established");
             try {
                 if (connection == null) {
                     connection = DriverManager.getConnection(JDBC_URL);
                     statement = connection.createStatement();
+
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
                 Assert.fail("Can't establish connection to DB");
+                log.info("Is not established connection ");
             }
         }
     @Test
@@ -36,20 +42,23 @@ public class JDBCTest {
 
         Assert.assertEquals("25.00",transactionAmount);
 
+
     }
     @After
     public void closeConnection() throws Exception{
         // 4. Close the connection
         connection.close();
+        log.info("Connection is closed");
     }
     public static String getTransactionAmount(String transactionNumber) {
         ResultSet result = executeQuery("select amount from digitalbank.account_transaction where transaction_number ='"+transactionNumber+"';");
         String amount = "";
+        log.info("Executed the query");
 
         try {
             result.next();
             amount = result.getString(1);
-            System.out.println(amount);
+            log.info("The amount queried is "+amount);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -67,6 +76,7 @@ public class JDBCTest {
         }
         return result;
     }
+
 }
 
 
